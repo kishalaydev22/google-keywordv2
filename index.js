8a18a80bd0d83d0d94744a214561b14726b2f2c0
kishalayCode <kishalaydev22@gmail.com>
2024-02-16 06:41:14 +0530

    updated server

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors'); // Import CORS middleware
-const cron = require('node-cron');
 const keyword = require('./routers/keyword');
 
 const app = express();
@@ -11,9 +10,9 @@ const port = 8080;
 app.use(express.json());
 
 app.use('/api/v1', cors(), keyword);
-cron.schedule('*/2 * * * *', () => {
-    console.log('Logging a message every 2 minutes');
-});
+app.get('/hello', (req, res) => {
+    res.status(200).send('server alive')
+})
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
