dd802f0cbf54f1b5096ae7b983c05faf88de70f2
kishalayCode <kishalaydev22@gmail.com>
2024-02-26 21:45:31 +0530

    updated run comand

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors'); // Import CORS middleware
 const keyword = require('./routers/keyword');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 
 // Enable CORS for all origins
