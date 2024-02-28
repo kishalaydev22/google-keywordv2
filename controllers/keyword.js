f5de7b3c5e76fc3f0f61aa88d62a9ded555b00d7
kishalayCode <kishalaydev22@gmail.com>
2024-02-28 11:17:11 +0530

    updated token

--- controllers/keyword.js
+++ controllers/keyword.js
@@ -13,7 +13,7 @@ const keywordVolume = async (query) => {
     try {
         const customer = client.Customer({
             customer_id: '3300180123',
-            refresh_token: '1//0gZ6-FhpkRpUXCgYIARAAGBASNwF-L9Ir74bUleabFmOGP37J2YbiF3XI5umHCXN5hDggrcu5jLxHsZNvlG6Rp8Pe2WlXBQj9D6Y',
+            refresh_token: '1//0guNhuvELzsdWCgYIARAAGBASNwF-L9Ir9HuZagXC_2qexnYMSYfg_ttpkAuaPNUyBqhOXgfo9H7jb5haD05PvQVVYXeWirHm168',
         });
         const keywordSeed = new services.KeywordSeed({ keywords: query });
         const generateKeywordIdeaResponse =
